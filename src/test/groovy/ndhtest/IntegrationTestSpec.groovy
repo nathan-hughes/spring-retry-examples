5cf65b9ef23537189e942d77edf4696a5ed6ffb3
Nathan Hughes <nathan.d.hughes@gmail.com>
2024-01-07 11:49:21 -0600

    remove unused import

--- src/test/groovy/ndhtest/IntegrationTestSpec.groovy
+++ src/test/groovy/ndhtest/IntegrationTestSpec.groovy
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.spockframework.spring.SpringBean
 import spock.lang.Subject
-import org.springframework.test.annotation.DirtiesContext
 
 @SpringBootTest(classes = [MyRetryableService, RandomNumberService])
 class IntegrationTestSpec extends Specification {
